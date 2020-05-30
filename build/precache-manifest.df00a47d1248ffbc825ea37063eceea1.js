689d3148eb31a9851d780910813662e6ea3dd2b5
Jerry <jerryx.xu@mail.utoronto.ca>
2020-05-30 18:37:31 -0400

    testing

---
+++ build/precache-manifest.df00a47d1248ffbc825ea37063eceea1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24828e27277274d7342a105f892b08e2",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "749f9061dc4f9addcc52",
+    "url": "/calculator/static/css/main.ce4c38f5.chunk.css"
+  },
+  {
+    "revision": "bda29df707973e724e1f",
+    "url": "/calculator/static/js/2.08714e50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.08714e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "749f9061dc4f9addcc52",
+    "url": "/calculator/static/js/main.23ebb9c5.chunk.js"
+  },
+  {
+    "revision": "958948d8790db65b6c69",
+    "url": "/calculator/static/js/runtime-main.fb6088dd.js"
+  }
+]);
