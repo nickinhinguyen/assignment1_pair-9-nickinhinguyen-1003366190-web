100a098a5d20b27a10a0dd4733beecaa807e224f
Jerry <jerryx.xu@mail.utoronto.ca>
2020-05-31 18:32:43 -0400

    edited comments and variable names

--- routes/API.js
+++ routes/API.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* A simple Api that receive a price, discount, tax and compute the result, return result back to user */
 router.post('/calculate', function(req, res, next) {
-  console.log(req.body)
   let total = req.body.price; 
   let discount = req.body.discount;
   let tax = req.body.tax;
-  console.log({price:total*tax*(1-discount)});
   res.send({price:total*tax*(1-discount)});
 });
 
--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
             {item:"/images/item5.jpg",price:50.00, count:0},
             {item:"/images/item6.jpg",price:20.00, count:0},
           ],
-          currentprice: 0,
+          initialprice: 0,
           finalprice: 0,
           error: false,
           errormessage: null,  
@@ -28,12 +28,11 @@ class App extends React.Component {
     }
 
     handleCountUpdate(e, itemname){
-        console.log("triggered")
+        // compute the new item list and initialprice before adding discount and taxes
         let item_list_copy = this.state.itemlist;
         let price = 0;
         for (let i = 0; i < item_list_copy.length; i++){
             if (item_list_copy[i].item === itemname){
-                console.log("triggered")
                 item_list_copy[i].count = e.target.value;
                 console.log(e.target.value)
             }
@@ -46,7 +45,7 @@ class App extends React.Component {
         }else{
           tax_multiplier = 1;
         }
-        // deal make the calculation;
+        // send to API to make the calculation;
         const request = new Request("/api/calculate", {
           method: 'post', 
           body: JSON.stringify({price:price, tax: tax_multiplier, discount: this.state.discount/100}),
@@ -67,8 +66,8 @@ class App extends React.Component {
         ) 
         .then(
           (res)=>{
-            console.log("price is ", res.price);
-            this.setState({itemlist: item_list_copy, currentprice:price, finalprice: res.price})
+            // update the view
+            this.setState({itemlist: item_list_copy, initialprice:price, finalprice: res.price})
           }
         )
         
@@ -76,7 +75,7 @@ class App extends React.Component {
 
 
     handleTaxChange(e){
-        
+      // find the tax rate
       let prev_tax_option = this.state.addtax;
       let tax_multiplier = 0;
       if (prev_tax_option){
@@ -84,10 +83,10 @@ class App extends React.Component {
       }else{
         tax_multiplier = 1.13;
       }
-      // send api to make the calculation
+      // send to api to make the calculation
       const request = new Request("/api/calculate", {
         method: 'post', 
-        body: JSON.stringify({price:this.state.currentprice, tax: tax_multiplier, discount: this.state.discount/100}),
+        body: JSON.stringify({price:this.state.initialprice, tax: tax_multiplier, discount: this.state.discount/100}),
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
@@ -105,12 +104,14 @@ class App extends React.Component {
       ) 
       .then(
         (res)=>{
+          // update the view
           this.setState({finalprice: res.price, addtax:!this.state.addtax})
         }
       )
     }
 
     handleDiscountChange(e){
+      // calculate the tax multiplier
       let tax_multiplier = 1.13;
       if (this.state.addtax){
         tax_multiplier = 1.13;
@@ -118,9 +119,10 @@ class App extends React.Component {
         tax_multiplier = 1;
       }
       let discount = e.target.value;
+      // send the request to API for final price
       const request = new Request("/api/calculate", {
         method: 'post', 
-        body: JSON.stringify({price:this.state.currentprice, tax: tax_multiplier, discount: discount/100}),
+        body: JSON.stringify({price:this.state.initialprice, tax: tax_multiplier, discount: discount/100}),
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
@@ -138,6 +140,7 @@ class App extends React.Component {
       ) 
       .then(
         (res)=>{
+          // Update the view
           this.setState({finalprice: res.price, discount:discount})
         }
       )
@@ -146,28 +149,28 @@ class App extends React.Component {
     render() {
         return (
                 <div style={{padding:'80px 20px'}}>
-                  <div class="col-md-8 col-xs-6 App-header-left font1" >Item </div>
-                  <div class="col-md-4 col-xs-6 App-header-right font1" style={{top:"0px"}}>Count</div>
+                  <div className="col-md-8 col-xs-6 App-header-left font1" >Item </div>
+                  <div className="col-md-4 col-xs-6 App-header-right font1" style={{top:"0px"}}>Count</div>
                   {this.state.itemlist.map(item => 
                     <div>
-                      <div class="col-md-8 col-xs-12 App-header-left"><img src={item.item} alt={item.item} style={{width:'100px'}}/> &nbsp; ${item.price.toFixed(2)}</div>
-                      <div class="col-md-4 col-xs-12 App-header-right"><input type="number" id="quantity" name="quantity" min="0" defaultValue="0" onChange={(e) => {this.handleCountUpdate(e, item.item)}}/></div>
+                      <div className="col-md-8 col-xs-12 App-header-left"><img src={item.item} alt={item.item} style={{width:'100px'}}/> &nbsp; ${item.price.toFixed(2)}</div>
+                      <div className="col-md-4 col-xs-12 App-header-right"><input type="number" id="quantity" name="quantity" min="0" defaultValue="0" onChange={(e) => {this.handleCountUpdate(e, item.item)}}/></div>
                     </div>)
                   }
-                  <div class="col-md-8 col-xs-6 App-header-left font1" ></div>
-                  <div class="col-md-4 col-xs-6 App-header-right font1" style={{top:'0px'}}>
-                    <div class="App-header-right" style={{top:'0px'}}>
+                  <div className="col-md-8 col-xs-6 App-header-left font1" ></div>
+                  <div className="col-md-4 col-xs-6 App-header-right font1" style={{top:'0px'}}>
+                    <div className="App-header-right" style={{top:'0px'}}>
                       <input type="checkbox" id="tax" name="tax" defaultChecked='True' onChange={(e) => {this.handleTaxChange(e)}}/>
                       <label for="tax" style={{fontWeight:200, color:'white', marginLeft:'15px'}}> Add taxes</label><br/>
                     </div>
                   </div>
-                  <div class="col-md-4 col-xs-6 App-header-right font1" style={{ top:'0px'}}>
-                    <div class="App-header-right" style={{top:'0px'}}>
+                  <div className="col-md-4 col-xs-6 App-header-right font1" style={{ top:'0px'}}>
+                    <div className="App-header-right" style={{top:'0px'}}>
                       Discount: <input type="number" id="discount" name="discount" min="0" defaultValue="0" onChange={(e) => {this.handleDiscountChange(e)}}/> %
                     </div>
                   </div>
-                  <div class="col-md-8 col-xs-6 App-header-left font1" ></div>
-                  <div class="col-md-4 col-xs-6 App-header-right font1" >Price Total <br/> ${this.state.finalprice.toFixed(2)}</div>
+                  <div className="col-md-8 col-xs-6 App-header-left font1" ></div>
+                  <div className="col-md-4 col-xs-6 App-header-right font1" >Price Total <br/> ${this.state.finalprice.toFixed(2)}</div>
                   
                 </div>
                 )
